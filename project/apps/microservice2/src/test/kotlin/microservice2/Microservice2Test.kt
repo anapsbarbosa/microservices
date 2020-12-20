c051f6d0bc7a32cadcfc701ae3c155fee5899132
Ana <ee.anap@gmail.com>
2020-12-20 17:48:32 -0300

    feat(microservice2): create microservice 2

---
+++ project/apps/microservice2/src/test/kotlin/microservice2/Microservice2Test.kt
@@ -0,0 +1,19 @@
+package microservice2
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class Microservice2Test {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
